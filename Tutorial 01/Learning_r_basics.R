e3375cfac7d92b7b49ddd9f98b47d8169b57c860
Nazhim <nazhimkalam@gmail.com>
2021-01-25 23:57:52 +0530

    commit

--- Tutorial 01/Learning_r_basics.R
+++ Tutorial 01/Learning_r_basics.R
@@ -6,3 +6,48 @@ print(23.9 + 11.6)
 
 myString <- "Hello, World!"
 print(myString)
+
+v <- TRUE
+print(class(v)) # logical
+
+v <- 23.5
+print(class(v)) # numeric
+
+v <- 2L        # we use L for the integer data type
+print(class(v))
+
+v <- "TRUE"    # character
+print(class(v))
+
+v <- charToRaw("Hello")  # raw ("Hello" is stored as 48 65 6c 6c 6f)
+print(class(v))
+
+# ----------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
