260617b6a0c8f20621623a6c2b3161b8010f4ba5
Nazhim <nazhimkalam@gmail.com>
2021-01-26 19:16:30 +0530

    completed strings in R

--- Tutorial 01/Learning_r_basics.R
+++ Tutorial 01/Learning_r_basics.R
@@ -436,15 +436,77 @@ myFunction(5, 3, 11)
 # Call the function by names of the arguments.
 myFunction(a=11, b=5, c=3)
 
+# ------------------------------------- R STRINGS -----------------------------
 
+# Strings in General
+a <- 'Start and end with single quote'
+print(a)
 
+b <- "Start and end with double quotes"
+print(b)
 
+c <- "single quote ' in between double quotes"
+print(c)
 
+d <- 'Double quotes " in between single quote'
+print(d)
 
+# Concatenating Strings - paste() function
+a <- "Hello"
+b <- 'How'
+c <- "are you? "
 
+print(paste(a,b,c))
 
+print(paste(a,b,c, sep = "-"))
 
+# collapse is used to eliminate the space in between two strings. 
+# But not the space within two words of one string.
+print(paste(a,b,c, sep = "", collapse = ""))
 
+# Formatting number & Strings - format() function
+# Numbers and strings can be formatted to a specific style using format()function.
 
+# Total number of digits displayed. Last digit rounded off.
+result <- format(23.123456789, digits = 7)
+print(result)
+
+# Display numbers in scientific notation.
+result <- format(c(6, 13.14521), scientific = TRUE)
+print(result)
+
+# The minimum number of digits to the right of the decimal point.
+result <- format(23.47, nsmall = 7)
+print(result)
+
+# Format treats everything as a string.
+result <- format(6)
+print(result)
+
+# Numbers are padded with blank in the beginning for width.
+result <- format(13.7, width = 6)
+print(result)
+
+# Counting number of characters in a string - nchar() function --------
+result <- nchar("Count the number of characters")
+print(result)
+
+# Changing the case - toupper() & tolower() functions
+x <- c('one', 'nazhim')
+toupper(x)
+tolower(x)
+
+# Changing to Upper case.
+result <- toupper("Changing To Upper")
+print(result)
+
+# Changing to lower case.
+result <- tolower("Changing To Lower")
+print(result)
+
+# Extracting parts of a string - substring() function
+# Extract characters from 5th to 7th position.
+result <- substring("Extract", 5, 7)
+print(result)
 
 
